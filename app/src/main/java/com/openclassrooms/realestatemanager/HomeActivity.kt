68aeb4913cbcade972fb755f9a2e6ed3ac3c2fb9
Execk <benjamin76510@hotmail.fr>
2019-07-03 11:50:18 +0200

    Add a toolbar menu

--- app/src/main/java/com/openclassrooms/realestatemanager/HomeActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.openclassrooms.realestatemanager
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.toolbar.*
 
@@ -14,4 +16,21 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun configureToolbar() = setSupportActionBar(toolbar)
+
+    /**
+     * Inflate the menu and add it to the Toolbar.
+     */
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when(item?.itemId) {
+            R.id.search_button -> { println("do something"); true }
+            R.id.create_button -> { println("do something"); true }
+            R.id.add_button -> { println("do something"); true }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
