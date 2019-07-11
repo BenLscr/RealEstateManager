761db696448009e4e6317546a952b501fe86bbda
Execk <benjamin76510@hotmail.fr>
2019-07-11 10:54:57 +0200

    Create AddressDao

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/database/dao/AddressDao.kt
@@ -0,0 +1,28 @@
+package com.openclassrooms.realestatemanager.database.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.openclassrooms.realestatemanager.models.Address
+
+@Dao
+interface AddressDao {
+
+    @Query("SELECT * FROM Address")
+    fun getAdresses(id: Int): LiveData<List<Address>>
+
+    @Query("SELECT * FROM Address WHERE id == id")
+    fun getAddress(id: Int): LiveData<Address>
+
+    @Insert
+    fun insertAddress(address: Address)
+
+    @Update
+    fun updateAddress(address: Address)
+
+    @Query("DELETE FROM Address WHERE id == id")
+    fun deleteAddress(id: Int)
+
+    /**@Delete("DELETE * FROM Address WHERE id == id")
+    fun deleteAddress(id: Int)*/
+
+}
