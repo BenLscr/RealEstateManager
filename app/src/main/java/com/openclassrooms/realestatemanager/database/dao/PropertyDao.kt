b314e614f9de34b614f50b46c56927bcf63adb3e
Execk <benjamin76510@hotmail.fr>
2019-07-11 10:55:30 +0200

    Create PropertyDao

---
+++ app/src/main/java/com/openclassrooms/realestatemanager/database/dao/PropertyDao.kt
@@ -0,0 +1,28 @@
+package com.openclassrooms.realestatemanager.database.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.openclassrooms.realestatemanager.models.Property
+
+@Dao
+interface PropertyDao {
+
+    @Query("SELECT * FROM Property")
+    fun getPropertys(id: Int): LiveData<List<Property>>
+
+    @Query("SELECT * FROM Property WHERE id == id")
+    fun getProperty(id: Int): LiveData<Property>
+
+    @Insert
+    fun insertProperty(property: Property)
+
+    @Update
+    fun updateProperty(property: Property)
+
+    @Query("DELETE FROM Property WHERE id == id")
+    fun deleteProperty(id: Int)
+
+    /**@Delete("DELETE * FROM Property WHERE id == id")
+    fun deleteProperty(id: Int)*/
+
+}
