13a34603432958b282cf9364bcd517786526c165
Execk <benjamin76510@hotmail.fr>
2019-09-05 15:04:42 +0200

    Implement goSearch in SearchFormActivity

--- app/src/main/java/com/openclassrooms/realestatemanager/form/searchForm/SearchFormActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/form/searchForm/SearchFormActivity.kt
@@ -4,14 +4,15 @@ import android.app.DatePickerDialog
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.CheckBox
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doAfterTextChanged
 import com.openclassrooms.realestatemanager.R
+import com.openclassrooms.realestatemanager.form.searchForm.models.SearchFormModelRaw
 import kotlinx.android.synthetic.main.activity_search_form.*
-import kotlinx.android.synthetic.main.form.*
 import kotlinx.android.synthetic.main.toolbar.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -19,25 +20,7 @@ import java.util.*
 class SearchFormActivity : AppCompatActivity() {
 
     private val calendar: Calendar = Calendar.getInstance()
-    private var district = ""
-    private var city = ""
-    private var postalCode = ""
-    private var country = ""
-    private var minPrice = ""
-    private var maxPrice = ""
-    private var type = ""
-    private var minSurface = ""
-    private var maxSurface = ""
-    private var rooms = ""
-    private var bathrooms = ""
-    private var bedrooms = ""
-    private var school = false
-    private var commerces = false
-    private var park = false
-    private var subways = false
-    private var train = false
-    private var availability: Boolean? = null
-    private var dateLong: Long = 0
+    private val searchFormModelRaw = SearchFormModelRaw()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,25 +38,25 @@ class SearchFormActivity : AppCompatActivity() {
 
     private fun fillEveryDropDownMenu() {
         //District
-        ArrayAdapter.createFromResource(this, R.array.form_district, R.layout.dropdown_menu_form_item
+        ArrayAdapter.createFromResource(this, R.array.search_district, R.layout.dropdown_menu_form_item
         ).also { adapter ->
             adapter.setDropDownViewResource(R.layout.dropdown_menu_form_item)
             search_district.setAdapter(adapter)
         }
         //City
-        ArrayAdapter.createFromResource(this, R.array.form_city, R.layout.dropdown_menu_form_item
+        ArrayAdapter.createFromResource(this, R.array.search_city, R.layout.dropdown_menu_form_item
         ).also { adapter ->
             adapter.setDropDownViewResource(R.layout.dropdown_menu_form_item)
             search_city.setAdapter(adapter)
         }
         //Country
-        ArrayAdapter.createFromResource(this, R.array.form_country, R.layout.dropdown_menu_form_item
+        ArrayAdapter.createFromResource(this, R.array.search_country, R.layout.dropdown_menu_form_item
         ).also { adapter ->
             adapter.setDropDownViewResource(R.layout.dropdown_menu_form_item)
             search_country.setAdapter(adapter)
         }
         //Type
-        ArrayAdapter.createFromResource(this, R.array.form_type, R.layout.dropdown_menu_form_item
+        ArrayAdapter.createFromResource(this, R.array.search_type, R.layout.dropdown_menu_form_item
         ).also { adapter ->
             adapter.setDropDownViewResource(R.layout.dropdown_menu_form_item)
             search_type.setAdapter(adapter)
@@ -81,39 +64,35 @@ class SearchFormActivity : AppCompatActivity() {
     }
 
     private fun addEveryListener() {
-        search_district.doAfterTextChanged { district = it.toString() }
-        search_city.doAfterTextChanged { city = it.toString() }
-        search_postal_code_edit_text.doAfterTextChanged { postalCode = it.toString() }
-        search_country.doAfterTextChanged { country = it.toString() }
-        search_price_min_edit_text.doAfterTextChanged { minPrice = it.toString() }
-        search_price_max_edit_text.doAfterTextChanged { maxPrice = it.toString() }
-        search_type.doAfterTextChanged { type = it.toString() }
-        search_min_surface_edit_text.doAfterTextChanged { minSurface = it.toString() }
-        search_max_surface_edit_text.doAfterTextChanged { maxSurface = it.toString() }
-        search_rooms_edit_text.doAfterTextChanged { rooms = it.toString() }
-        search_bathrooms_edit_text.doAfterTextChanged { bathrooms = it.toString() }
-        search_bedrooms_edit_text.doAfterTextChanged { bedrooms = it.toString() }
-        search_radio_available.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                availability = true
+        with(searchFormModelRaw) {
+            search_district.doAfterTextChanged { district = it.toString() }
+            search_city.doAfterTextChanged { city = it.toString() }
+            search_postal_code_edit_text.doAfterTextChanged { postalCode = it.toString() }
+            search_country.doAfterTextChanged { country = it.toString() }
+            search_price_min_edit_text.doAfterTextChanged { minPrice = it.toString() }
+            search_price_max_edit_text.doAfterTextChanged { maxPrice = it.toString() }
+            search_type.doAfterTextChanged { type = it.toString() }
+            search_min_surface_edit_text.doAfterTextChanged { minSurface = it.toString() }
+            search_max_surface_edit_text.doAfterTextChanged { maxSurface = it.toString() }
+            search_rooms_edit_text.doAfterTextChanged { rooms = it.toString() }
+            search_bathrooms_edit_text.doAfterTextChanged { bathrooms = it.toString() }
+            search_bedrooms_edit_text.doAfterTextChanged { bedrooms = it.toString() }
+            search_radio_available.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) { availability = true }
+            }
+            search_radio_sold.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) { availability = false }
+            }
+            search_radio_indifferent.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) { availability = null }
+            }
+            search_radio_indifferent.isChecked = true
+            search_clear_date.setOnClickListener {
+                search_select_date.text = getString(R.string.select_a_date)
+                dateLong = 0
             }
         }
-        search_radio_sold.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                availability = false
-            }
-        }
-        search_radio_indifferent.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                availability = null
-            }
-        }
-        search_radio_indifferent.isChecked = true
         search_select_date.setOnClickListener { initDatePickerDialog() }
-        search_clear_date.setOnClickListener {
-            search_select_date.text = getString(R.string.select_a_date)
-            dateLong = 0
-        }
         search_button.setOnClickListener { goSearch() }
         search_cancel_button.setOnClickListener { finish() }
     }
@@ -136,19 +115,63 @@ class SearchFormActivity : AppCompatActivity() {
         val visualFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
         val date = visualFormat.format(calendar.time)
         search_select_date.text = date
-        dateLong = calendar.timeInMillis
+        searchFormModelRaw.dateLong = calendar.timeInMillis
     }
 
     fun onCheckboxClicked(view: View) {
         when(view as CheckBox) {
-            search_school_checkbox -> school = search_school_checkbox.isChecked
-            search_commerces_checkbox -> commerces = search_commerces_checkbox.isChecked
-            search_park_checkbox -> park = search_park_checkbox.isChecked
-            search_subways_checkbox -> subways = search_subways_checkbox.isChecked
-            search_train_checkbox -> train = search_train_checkbox.isChecked
+            search_school_checkbox -> searchFormModelRaw.school = search_school_checkbox.isChecked
+            search_commerces_checkbox -> searchFormModelRaw.commerces = search_commerces_checkbox.isChecked
+            search_park_checkbox -> searchFormModelRaw.park = search_park_checkbox.isChecked
+            search_subways_checkbox -> searchFormModelRaw.subways = search_subways_checkbox.isChecked
+            search_train_checkbox -> searchFormModelRaw.train = search_train_checkbox.isChecked
         }
     }
 
-    private fun goSearch() {}
+    private fun goSearch() {
+        with(searchFormModelRaw) {
+            if (minPrice.isNotEmpty()
+                    || maxPrice.isNotEmpty()
+                    || type.isNotEmpty()
+                    || minSurface.isNotEmpty()
+                    || maxSurface.isNotEmpty()
+                    || rooms.isNotEmpty()
+                    || bathrooms.isNotEmpty()
+                    || bedrooms.isNotEmpty()
+                    || availability != null
+                    || dateLong > 0) {
+                if (school
+                        || commerces
+                        || park
+                        || subways
+                        || train) {
+                    Log.e("goSearch", "property && locInt")
+                } else {
+                    Log.e("goSearch", "property")
+                }
+            } else if (district.isNotEmpty()
+                    || city .isNotEmpty()
+                    || postalCode.isNotEmpty()
+                    || country.isNotEmpty()) {
+                if (school
+                        || commerces
+                        || park
+                        || subways
+                        || train) {
+                    Log.e("goSearch", "address && locInt")
+                } else {
+                    Log.e("goSearch", "address")
+                }
+            } else if (school
+                    || commerces
+                    || park
+                    || subways
+                    || train) {
+                Log.e("goSearch", "locInt")
+            } else {
+                Log.e("goSearch", "nothing filled")
+            }
+        }
+    }
 
 }
