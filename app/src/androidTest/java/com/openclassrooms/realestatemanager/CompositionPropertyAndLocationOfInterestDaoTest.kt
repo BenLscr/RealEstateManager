6acc1a5e2dc24f000b07185679fe51138378bb68
Execk <benjamin76510@hotmail.fr>
2019-09-11 13:16:21 +0200

    Add a new test class for CompositionPropertyAndLocationOfInterestDao. Add a test for insert and get.

--- app/src/androidTest/java/com/openclassrooms/realestatemanager/CompositionPropertyAndLocationOfInterestDaoTest.kt
+++ app/src/androidTest/java/com/openclassrooms/realestatemanager/CompositionPropertyAndLocationOfInterestDaoTest.kt
@@ -2,16 +2,12 @@ package com.openclassrooms.realestatemanager
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
-import androidx.test.espresso.matcher.ViewMatchers.assertThat
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.platform.app.InstrumentationRegistry
 import com.openclassrooms.realestatemanager.database.AppDatabase
-import com.openclassrooms.realestatemanager.models.Address
-import com.openclassrooms.realestatemanager.models.City
-import com.openclassrooms.realestatemanager.models.Country
-import com.openclassrooms.realestatemanager.models.District
+import com.openclassrooms.realestatemanager.models.CompositionPropertyAndLocationOfInterest
+import com.openclassrooms.realestatemanager.models.LocationOfInterest
 import junit.framework.Assert.assertTrue
-import org.hamcrest.CoreMatchers.equalTo
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -20,7 +16,7 @@ import org.junit.runner.RunWith
 import java.io.IOException
 
 @RunWith(AndroidJUnit4ClassRunner::class)
-class AddressDaoTest {
+class CompositionPropertyAndLocationOfInterestDaoTest {
 
     private lateinit var db: AppDatabase
 
@@ -42,24 +38,16 @@ class AddressDaoTest {
         db.close()
     }
 
-    /*@Test
+    @Test
     @Throws(Exception::class)
-    fun updateAndGetAddress() {
-        val USER_ID = 3
-        val address = Address(
-                id = USER_ID,
-                path = "451, rue Robert Lefranc",
-                complement = null,
-                district = District.STATEN_ISLAND,
-                city = City.NEW_YORK,
-                postalCode = "76510",
-                country = Country.UNITED_STATES)
-        db.addressDao().insertAddress(address)
+    fun insertAndGetCompositionFromDatabase() {
+        val propertyId = 65
+        val locationOfInterestId = LocationOfInterest.PARK.ordinal
+        val composition = CompositionPropertyAndLocationOfInterest(propertyId, locationOfInterestId)
+        db.compositionPropertyAndLocationOfInterestDao().insertLocationOfInterest(composition)
+        val getComposition = LiveDataTestUtil.getValue(db.compositionPropertyAndLocationOfInterestDao().getLocationsOfInterest(propertyId))
 
-        val getAddress: Address? = LiveDataTestUtil.getValue(db.addressDao().getAddress(USER_ID))
-
-        assertTrue(getAddress?.path == "451, rue Robert Lefranc" && getAddress.id == USER_ID)
-        //assertThat(getAddress?.path, equalTo(address.path))
-    }*/
+        assertTrue(getComposition?.get(0)?.propertyId == composition.propertyId && getComposition[0].locationOfInterestId == composition.locationOfInterestId )
+    }
 
 }
