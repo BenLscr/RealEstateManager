76a7d33939d4f764d1831aa339716df35989b7d3
Execk <benjamin76510@hotmail.fr>
2019-09-15 01:02:07 +0200

    First commit after a big bug from a blue screen .. (love this)

--- app/src/main/java/com/openclassrooms/realestatemanager/HomeActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/HomeActivity.kt
@@ -5,10 +5,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
